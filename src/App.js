89da5852e05323d8e4a059be72478d238e09ad9d
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-06-27 11:07:53 +0200

    zbudowano prototyp podstrony do rutingu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Layout from "./meta/Layout";
-import Register from "./pages/Register";
+import SmallMeme from "./components/SmallMeme";
+
 
 function App() {
 
     return (
     <Layout>
         <h1>Witaj na stronie o memach!</h1>
-        <Register/>
     </Layout>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,27 +3,30 @@ import {Button, Form} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "../form.css";
 
-const Login =()=> {
+const Login =({location})=> {
     return (
-        <Form id={"login"}>
-            <h3>Zaloguj się</h3>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Adres email</Form.Label>
-                <Form.Control type="email" placeholder="np. jan.kowalski@gmail.com" />
-                <Form.Text className="text-muted">
-                    Twoje dane będą zawsze chronione
-                </Form.Text>
-            </Form.Group>
+        <Layout>
+            <h1>Strona logowania : <span>{location.pathname}</span></h1>
+            <Form id={"login"}>
+                <h3>Zaloguj się</h3>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Adres email</Form.Label>
+                    <Form.Control type="email" placeholder="np. jan.kowalski@gmail.com" />
+                    <Form.Text className="text-muted">
+                        Twoje dane będą zawsze chronione
+                    </Form.Text>
+                </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Hasło</Form.Label>
-                <Form.Control type="password" placeholder="podaj hasło" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Zapamiętaj mnie" />
-            </Form.Group>
-            <Button variant={"primary"}>Zaloguj się</Button>
-        </Form>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Hasło</Form.Label>
+                    <Form.Control type="password" placeholder="podaj hasło" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Zapamiętaj mnie" />
+                </Form.Group>
+                <Button variant={"primary"}>Zaloguj się</Button>
+            </Form>
+        </Layout>
     );
 }
 
