2beb55665205d791f76c490a040fa22c6c16e794
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-06-27 10:54:30 +0200

    poprawiono importy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
 import Layout from "./meta/Layout";
 import Register from "./pages/Register";
-<<<<<<< HEAD
-=======
-import MyComponent from "./components/MyComponent";
-import Meme from "./components/Meme";
-import GetMemes from "./meta/GetMemes";
-import SmallMeme from "./components/SmallMeme";
-import {Pagination} from "react-bootstrap";
-import Comments from "./components/Comments";
->>>>>>> 8681d990bd80d4ab74686b8f66cd857633fe1e27
 
 function App() {
 
