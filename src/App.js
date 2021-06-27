aa8decfd250b09c50bc4ecd014c6a28244d59faa
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-06-27 10:46:59 +0200

    czyszczenie kodu z niepotrzebnych importów

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
 import Layout from "./meta/Layout";
-import MemeTripleView from "./containers/MemeTripleView"
-import Home from "./containers/Home";
-import Login from "./pages/Login";
 import Register from "./pages/Register";
-import MyComponent from "./components/MyComponent";
-import Meme from "./components/Meme";
-import GetMemes from "./meta/GetMemes";
-import SmallMeme from "./components/SmallMeme";
-import {Pagination} from "react-bootstrap";
 
 function App() {
-
-    const props = {"id": 1};
-    const meme = GetMemes(props);
 
     return (
     <Layout>
         <h1>Witaj na stronie o memach!</h1>
-        <MyComponent/>
+        <Register/>
     </Layout>
   );
 }
