420ba8186f84a88147654b15975d6b1f99b5221e
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2021-06-27 11:17:19 +0200

    poprawiono element pojedynczego mema na losowy mem (SingularView -> RandomMeme)

--- src/pages/RandomMeme.js
+++ src/pages/RandomMeme.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Layout from "../meta/Layout";
 
-/*
-This component provides with view of single meme
+/***
+ *
+ * @param location is a path provided by router
+ * @returns {JSX.Element} is an actual element
  */
-const SingleView = ({location}) => {
+const RandomMeme = ({location}) => {
     return (
         <Layout>
             <h1>To jest strona dla pojedynczego mema. <span>{location.pathname}</span></h1>
             <p>Poniżej musi znajdować się komponent dyskusji (listy komentarzy)</p>
         </Layout>
-    )
+    );
 }
+
+export default RandomMeme;
