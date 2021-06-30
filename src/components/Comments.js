d23c03adb9189fba66925f220b923fd5e65de2e4
kamil100900 <56301469+kamil100900@users.noreply.github.com>
2021-06-30 10:49:54 +0200

    poprawienie komponentu komentarzy

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,19 +1,53 @@
 import React from "react";
-import {Card} from "react-bootstrap";
+import {Button, Card, ListGroup, ListGroupItem} from "react-bootstrap";
 
-const Comments =({comment})=> {
-    return (<Card>
-            <Card.Title>
-                {comment.author_id}
-            </Card.Title>
-            <Card.Body>
-                {comment.added_at}
-                {comment.likes}
-                {comment.content}
-                {comment.meme_id}
-            </Card.Body>
-        </Card>
-    );
+class Comments extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            data: []
+        }
+    }
+    async componentDidMount() {
+        await fetch('https://s108.labagh.pl/comments/',
+            {
+                Method: "GET",
+                headers: new Headers({
+                    Accept: "*/*"
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({isLoaded: true});
+                this.setState({data: data});
+            });
+    }
+    render() {
+        const {isLoaded, data} = this.state;
+        return (
+            isLoaded ?
+                <div id={"all-comments"}>
+                    <h1>Komentarze</h1>
+                    <ListGroup>
+                        {data.map( item => (
+                            <ListGroupItem id={"item"}>
+                                <h4 id={"author"}>{item.author_id}</h4>
+                                <p id={"content"}>{item.content}</p>
+                                <Button>
+                                    <img src={"../img/thumbs_up.png"} alt={""}/>
+                                </Button>
+                            </ListGroupItem>
+                        ))}
+                    </ListGroup>
+                    <br/>
+                </div>
+                :
+                <p>Loading ...</p>
+        );
+    }
 }
+
 
 export default Comments;
